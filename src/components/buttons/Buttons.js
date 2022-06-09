0c6364143b9aaa67ec8281a6058b7ac5f4d7f01a
Alizz M <alizz.mejia03@gmail.com>
2022-06-09 13:58:17 -0500

    add footer

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,10 +1,10 @@
 // import { Link } from 'react-router-dom';
 import './buttons.css';
 
-function Btn({ texto }) {
+function Btn({ texto, href }) {
     return (
         <div className='button-container'>
-            <a href="https://api.whatsapp.com/send?phone=573113071168&text=Hola%20Aliz!!%20Estoy%20interesad%40%20en%20contratar%20tus%20servicios%20de%20Developer%20front%20End"target="_blank" rel="noreferrer" className='btn-az'>
+            <a href={href}target="_blank" rel="noreferrer" className='btn-az'>
                 <span></span>
                 <span></span>
                 <span></span>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import Navbar from "../components/navbar/Navbar";
 import ilustrator from '../components/navbar/img/Melogo.png'
 import '../index.css'
 import Btn from '../components/buttons/Buttons'
+import Footer from "../components/footer/Footer";
 
 
 function Home() {
@@ -14,12 +15,13 @@ function Home() {
                         <section>
                             <span className="wrapper"><h1 className="typing-demo">Hi i’m <strong>Aliz</strong></h1></span>
                             <h3>freelance web <br /> developer</h3>
-                            <Btn texto="Hire me"></Btn>
+                            <Btn href="https://api.whatsapp.com/send?phone=573113071168&text=Hola%20Aliz!!%20Estoy%20interesad%40%20en%20contratar%20tus%20servicios%20de%20Developer%20front%20End" texto="Hire me"></Btn>
                         </section>
                     </div>
                     <div className="container-Home-img"><img src={ilustrator} alt="caracter" /></div>
                 </div>
             </section>
+            <Footer></Footer>
         </div >
     );
 }
